7e4a33bdd87dfcaa2bd9ee906f99bdf8b6404d36
norbidrak <norbipep@interia.pl>
2017-06-22 19:53:03 +0200

    Update script.js

--- costam/scripts/script.js
+++ costam/scripts/script.js
@@ -1,2 +1,13 @@
 
-alert('dfdf');
+$(function () {
+
+    $.ajax({
+        url: 'data.json',
+        method: 'GET',
+        success: function (data) {
+
+         console.log(data);
+
+        }
+    });
+});
