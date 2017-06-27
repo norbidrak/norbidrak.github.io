8fee53457f3083f59ab02dea5b3fda906660df1e
norbidrak <norbipep@interia.pl>
2017-06-27 13:18:55 +0200

    Update script.js

--- costam/scripts/script.js
+++ costam/scripts/script.js
@@ -1,13 +1,29 @@
 
 $(function () {
+    var backgrounds = ['url(../images/banner.jpg)', 'url(../images/banner2.jpg)', 'url(../images/banner3.jpg)'];
+    var current = 0;
+    var timer;
 
-    $.ajax({
-        url: 'data.json',
-        method: 'GET',
-        success: function (data) {
+    function nextBackground() { //funkcja zmieniająca obrazku w sliderze
 
-         console.log(data);
+        $(".masthead").stop().animate({
+                opacity: 0
+            }, 0).css({
+                'background': backgrounds[current] + " 50% 80% /cover no-repeat fixed"
+            })
+            .stop().animate({
+                opacity: 1
+            }, 2000);
 
-        }
-    });
+        timer = setTimeout(function () {
+            current++;
+            if (current === 3) {
+                current = 0;
+            }
+            nextBackground();
+
+        }, 5000);
+    }
+
+    nextBackground();
 });
